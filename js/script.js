609e0b9ede6258534330beea3fabe9dbf6f52c72
mrxanti <mrxanti@mail.ru>
2022-11-24 00:36:39 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+function scrollToHandler_serv_1() {
+    window.scrollTo({
+        top: 900,
+        behavior: "smooth"
+    });
+}
+function scrollToHandler_serv_2() {
+    window.scrollTo({
+        top: 2800,
+        behavior: "smooth"
+    });
+}
+function scrollToHandler_serv_3() {
+    window.scrollTo({
+        top: 1830,
+        behavior: "smooth"
+    });
+}
+function scrollToHandler_serv_4() {
+    window.scrollTo({
+        top: 3700,
+        behavior: "smooth"
+    });
+}
+function scrollToHandler_term_1() {
+    window.scrollTo({
+        top: 1000,
+        behavior: "smooth"
+    });
+}
+function scrollToHandler_term_2() {
+    window.scrollTo({
+        top: 2400,
+        behavior: "smooth"
+    });
+}
+function scrollToHandler_term_3() {
+    window.scrollTo({
+        top: 3300,
+        behavior: "smooth"
+    });
+}
+function scrollToHandler_term_4() {
+    window.scrollTo({
+        top: 600,
+        behavior: "smooth"
+    });
+}
+function scrollToHandler_term_5() {
+    window.scrollTo({
+        top: 2550,
+        behavior: "smooth"
+    });
+}
+function scrollToHandler_term_6() {
+    window.scrollTo({
+        top: 3900,
+        behavior: "smooth"
+    });
+}
